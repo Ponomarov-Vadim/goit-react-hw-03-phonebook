7d77ac831e88d5fcfe422403417209998daafe62
Vadim Ponomarov <remary300@gmail.com>
2020-05-23 11:27:12 +0300

    added local storage to phonebook

---
+++ src/components/ContactForm/index.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import styled from "./ContactForm.module.css";
+
+import PropTypes from "prop-types";
+import classNames from "classnames";
+
+const initialState = {
+  name: "",
+  number: "",
+};
+
+const isTelNumber = (tel) => !Number.isNaN(Number(tel.split("-").join("")));
+
+export default class ContactForm extends Component {
+  state = { ...initialState };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      this.props.contacts.find(
+        (contact) => contact.name === this.state.name
+      ) === undefined
+    ) {
+      if (isTelNumber(this.state.number)) {
+        this.props.addContact(this.state.name, this.state.number);
+      } else {
+        alert(`"${this.state.number}" is incorrect telephone number`);
+      }
+    } else {
+      alert(`"${this.state.name}" is alredy in contacts`);
+    }
+
+    this.setState({ ...initialState });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className={classNames(styled.form)}>
+        <h3>Name</h3>
+        <input
+          type="text"
+          name="name"
+          className={classNames(styled.input)}
+          style={{ display: "block", marginBottom: 10 }}
+          onChange={this.handleChange}
+          value={this.state.name}
+        />
+
+        <h3>Number</h3>
+        <input
+          type="text"
+          name="number"
+          className={classNames(styled.input)}
+          onChange={this.handleChange}
+          value={this.state.number}
+        />
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  addContact: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+};
